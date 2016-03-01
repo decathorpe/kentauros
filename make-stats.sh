0a18517dbc52e40326040814848fbf0c8523f698
Fabio Valentini <decathorpe@gmail.com>
2016-03-01 15:59:53 +0100

    add statistics tracking

---
+++ make-stats.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/bash
+
+# generate pylint statistics
+python3-pylint -f html ./ktr ./kentauros > stats/pylint_stats.html
+
+cd ./stats
+
+# generate cloc statistics
+cloc --xml --xsl=1 --exclude-dir=../stats --exclude-ext=html,xml,xsl --out=./cloc.xml ../
