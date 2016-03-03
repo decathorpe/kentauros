df6e64cc23f76f0d1fd18d69df055422289c285e
Fabio Valentini <decathorpe@gmail.com>
2016-03-03 15:26:31 +0100

    stats/cloc: exclude examples directory

--- make-stats.sh
+++ make-stats.sh
@@ -6,4 +6,6 @@ python3-pylint -f html ./ktr ./kentauros > stats/pylint_stats.html
 cd ./stats
 
 # generate cloc statistics
-cloc --xml --xsl=1 --exclude-dir=../stats --exclude-ext=html,xml,xsl --out=./cloc.xml ../
+cloc --xml --xsl=1 --exclude-dir=../stats --exclude-dir=../examples \
+    --exclude-ext=html,xml,xsl --out=./cloc.xml ../
+
