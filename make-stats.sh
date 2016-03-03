5da135fbd807f39e9b5d5b564e47e510747342b3
Fabio Valentini <decathorpe@gmail.com>
2016-03-03 22:58:53 +0100

    stats: remove cloc stats, cloc seems to be broken on f24

--- make-stats.sh
+++ make-stats.sh
@@ -3,9 +3,8 @@
 # generate pylint statistics
 python3-pylint -f html ./ktr ./kentauros > stats/pylint_stats.html
 
-cd ./stats
+# cd ./stats
 
 # generate cloc statistics
-cloc --xml --xsl=1 --exclude-dir=../stats --exclude-dir=../examples \
-    --exclude-ext=html,xml,xsl --out=./cloc.xml ../
-
+#cloc --xml --xsl=1 --exclude-dir=../stats --exclude-dir=../examples \
+#    --exclude-ext=html,xml,xsl --out=./cloc.xml ../
