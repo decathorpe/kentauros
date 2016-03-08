c317befe34594f2f80322d766f23f58d8834c09b
Fabio Valentini <decathorpe@gmail.com>
2016-03-08 18:19:31 +0100

    docs: add intersphinx, set theme to RTD-style theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -35,6 +35,7 @@ extensions = [
     'sphinx.ext.todo',
     'sphinx.ext.coverage',
     'sphinx.ext.ifconfig',
+    'sphinx.ext.intersphinx',
     'sphinx.ext.napoleon',
     'sphinx.ext.viewcode',
 ]
@@ -116,7 +117,7 @@ todo_include_todos = True
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -294,4 +295,6 @@ texinfo_documents = [
 # napoleon_use_ivar = True
 napoleon_numpy_docstring = False
 napoleon_use_rtype = False
+
+intersphinx_mapping = {'python': ('https://docs.python.org/3.5', None)}
 
