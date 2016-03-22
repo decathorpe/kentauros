4fc8700549fcae675b257d69af59ba9fecbf96db
Fabio Valentini <decathorpe@gmail.com>
2016-03-22 12:12:32 +0100

    ktr/config/envvar: refactor to be consistent - use absolute paths, _always_ - move global variables inside a function, so import has no side effects

--- kentauros/config/envvar.py
+++ kentauros/config/envvar.py
@@ -1,8 +1,10 @@
 """
-kentauros.config.envvar file
-reads environment variables to eventually determine location of
-- configuration files (confdir)
-- source directories / tarballs (datadir)
+This module contains :py:func:`get_env_config` function, which parses
+environment variables for settings. Those settings include - at the moment -
+the base directory for kentauros data (`KTR_BASEDIR`), the directory for package
+configuration files (`KTR_CONFDIR`), the directory for package sources
+(`KTR_DATADIR`), the directory for buildable source packages (`KTR_PACKDIR`)
+and the package specification directory (`KTR_SPECDIR`).
 """
 
 import os
@@ -11,50 +13,54 @@ from kentauros.definitions import KtrConfType
 from kentauros.config.common import KtrConf
 
 
-ENV_BASEDIR = os.environ.get("KTR_BASE_DIR")
-ENV_CONFDIR = os.environ.get("KTR_CONF_DIR")
-ENV_DATADIR = os.environ.get("KTR_DATA_DIR")
-ENV_PACKDIR = os.environ.get("KTR_PACK_DIR")
-ENV_SPECDIR = os.environ.get("KTR_SPEC_DIR")
-
-
 LOGPREFIX1 = "ktr/config/envvar: "
+"""This string specifies the prefix for log and error messages printed to
+stdout or stderr from inside this subpackage.
+"""
 
 
 def get_env_config():
     """
+    # TODO: napoleon docstring
     kentauros.config.envvar.get_env_config():
     function that returns a KtrConf object containing ENV settings
     """
+
+    env_basedir = os.environ.get("KTR_BASE_DIR")
+    env_confdir = os.environ.get("KTR_CONF_DIR")
+    env_datadir = os.environ.get("KTR_DATA_DIR")
+    env_packdir = os.environ.get("KTR_PACK_DIR")
+    env_specdir = os.environ.get("KTR_SPEC_DIR")
+
     # if no settings were set by env variables, return None
-    if (ENV_BASEDIR is None) and \
-       (ENV_CONFDIR is None) and \
-       (ENV_DATADIR is None) and \
-       (ENV_PACKDIR is None) and \
-       (ENV_SPECDIR is None):
+    if (env_basedir is None) and \
+       (env_confdir is None) and \
+       (env_datadir is None) and \
+       (env_packdir is None) and \
+       (env_specdir is None):
         return None
 
     # if at least basedir has been set, construct KtrConf from CLI switches
-    if ENV_BASEDIR != None:
+    if env_basedir != None:
         result = KtrConf(KtrConfType.ENV,
-                         basedir=os.path.abspath(ENV_BASEDIR))
+                         basedir=os.path.abspath(env_basedir))
 
-        if ENV_CONFDIR is not None:
-            result.confdir = ENV_CONFDIR
-        if ENV_DATADIR is not None:
-            result.datadir = ENV_DATADIR
-        if ENV_PACKDIR is not None:
-            result.packdir = ENV_PACKDIR
-        if ENV_SPECDIR is not None:
-            result.specdir = ENV_SPECDIR
+        if env_confdir is not None:
+            result.confdir = os.path.abspath(env_confdir)
+        if env_datadir is not None:
+            result.datadir = os.path.abspath(env_datadir)
+        if env_packdir is not None:
+            result.packdir = os.path.abspath(env_packdir)
+        if env_specdir is not None:
+            result.specdir = os.path.abspath(env_specdir)
 
     # basedir not set: all other dirs must be specified
     else:
         result = KtrConf(KtrConfType.ENV)
-        result.confdir = ENV_CONFDIR
-        result.datadir = ENV_DATADIR
-        result.packdir = ENV_PACKDIR
-        result.specdir = ENV_SPECDIR
+        result.confdir = os.path.abspath(env_confdir)
+        result.datadir = os.path.abspath(env_datadir)
+        result.packdir = os.path.abspath(env_packdir)
+        result.specdir = os.path.abspath(env_specdir)
 
     if result.validate():
         return result
