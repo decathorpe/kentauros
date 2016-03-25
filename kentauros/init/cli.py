ac167b46e6833306d515deec91e25633974c1c35
Fabio Valentini <decathorpe@gmail.com>
2016-03-25 22:19:06 +0100

    ktr/init/cli: update docstrings for complete documentation. update stats.

--- kentauros/init/cli.py
+++ kentauros/init/cli.py
@@ -1,6 +1,8 @@
 """
 This module contains functions for constructing an `argparse` command line
-argument parser, and parsing the determined command line arguments.
+argument parser, and parsing the determined command line arguments. It also
+contains classes which provide methods that return parsed CLI arguments,
+depending on where the `kentauros` module is used from.
 """
 
 import argparse
@@ -10,7 +12,28 @@ from kentauros.definitions import ActionType, KtrConfType, InstanceType
 
 def get_cli_parser_base():
     """
-    # TODO: napoleon docstring
+    This function constructs and returns a basic parser for command line
+    arguments, which will be further processed and added to by other functions.
+
+    The arguments parsed by this basic parser include:
+
+    * ``--debug`` (``-d``) switch to enable debug messages in kentauros and for
+      subprocesses
+    * ``--verbose`` (``-v``, ``-vv``) switch to control how many informational
+      messages will be printed (twice for extra verbosity)
+    * ``--basedir=BASEDIR`` argument to set base directory for kentauros files
+      (optional)
+    * ``--confdir=CONFDIR`` argument for directly specifying the directory which
+      contains package configuration files (optional)
+    * ``--datadir=DATADIR`` argument for directly specifying the directory which
+      contains package sources (optional)
+    * ``--packdir=PACKDIR`` argument for directly specifying the directory which
+      contains constructed buildable pre-packages (optional)
+    * ``--specdir=SPECDIR`` argument for directly specifying the directory which
+      contains instructions for building binaries from source (optional)
+
+    Returns:
+        argparse.ArgumentParser: basic CLI argument parser
     """
 
     cliparser = argparse.ArgumentParser(
@@ -83,7 +106,22 @@ def get_cli_parser_base():
 
 def get_cli_parser_normal(cliparser):
     """
-    # TODO: napoleon docstring
+    This function constructs and returns a parser for command line arguments,
+    which is used for "normal" execution (e.g. invoking the ``ktr`` script).
+
+    The arguments parsed by this parser include those from the basic parser,
+    and, additionally:
+
+    * package name(s) to specify which packages to process explicitly
+    * ``--all`` (``-a``) switch to enable processing all packages in ``CONFDIR``
+    * ``--force`` (``-f``) switch to force actions which would not be executed
+
+    Arguments:
+        argparse.ArgumentParser cliparser: basic argument parser got from
+                                           :py:func:`get_cli_parser_base()`
+
+    Returns:
+        argparse.ArgumentParser: CLI argument parser for ``ktr`` script
     """
 
     assert isinstance(cliparser, argparse.ArgumentParser)
@@ -202,7 +240,25 @@ def get_cli_parser_normal(cliparser):
 
 def get_cli_parser_config(cliparser):
     """
-    # TODO: napoleon docstring
+    This function constructs and returns a parser for command line arguments,
+    which is used when changing package settings (e.g. by invoking the
+    ``ktr-config`` script).
+
+    The arguments parsed by this parser include those from the basic parser,
+    and, additionally:
+
+    * package name(s) to specify which packages to process explicitly
+    * ``--all`` (``-a``) switch to enable processing all packages in ``CONFDIR``
+    * ``--section`` (``-s``) argument to specify section of config file
+    * ``--key`` (``-k``) argument to specify key in section of config file
+    * ``--value`` (``-V``) argument to specify the value to be written
+
+    Arguments:
+        argparse.ArgumentParser cliparser: basic argument parser got from
+                                           :py:func:`get_cli_parser_base()`
+
+    Returns:
+        argparse.ArgumentParser: CLI argument parser for ``ktr-config`` script
     """
 
     assert isinstance(cliparser, argparse.ArgumentParser)
@@ -241,7 +297,21 @@ def get_cli_parser_config(cliparser):
 
 def get_cli_parser_create(cliparser):
     """
-    # TODO: napoleon docstring
+    This function constructs and returns a parser for command line arguments,
+    which is used when creating a new package from template (not used yet).
+
+    The arguments parsed by this parser include those from the basic parser,
+    and, additionally:
+
+    * package name(s) to specify which packages to create
+    * ``--force`` (``-f``) switch to force action
+
+    Arguments:
+        argparse.ArgumentParser cliparser: basic argument parser got from
+                                           :py:func:`get_cli_parser_base()`
+
+    Returns:
+        argparse.ArgumentParser: CLI argument parser for ``ktr-create`` script
     """
 
     assert isinstance(cliparser, argparse.ArgumentParser)
@@ -264,7 +334,18 @@ def get_cli_parser_create(cliparser):
 
 def get_cli_parser(instance_type=InstanceType.NORMAL):
     """
-    # TODO: napoleon docstring
+    This function returns a parser for command line arguments with certain
+    switches and arguments, depending on which kentauros instance it is used
+    for.
+
+    Arguments:
+        InstanceType instance_type: invocation type (which script) - this
+                                    determines which CLI switches and arguments
+                                    will be available and defaults to normal
+                                    invocation
+
+    Returns:
+        argparse.ArgumentParser: CLI argument parser for the specified instance
     """
 
     assert isinstance(instance_type, InstanceType)
@@ -281,7 +362,19 @@ def get_cli_parser(instance_type=InstanceType.NORMAL):
 
 def get_parsed_cli(instance_type=InstanceType.NORMAL):
     """
-    # TODO: napoleon docstring
+    This function returns a `Namespace` object which contains the parsed CLI
+    switches and arguments, as specified in the :py:class:`ArgumentParser`
+    constructing functions in this module - and also depending on the instance
+    type specified.
+
+    Arguments:
+        InstanceType instance_type: invocation type (which script) - this
+                                    determines which CLI switches and arguments
+                                    will be available and defaults to normal
+                                    invocation
+
+    Returns:
+        Namespace: parsed CLI arguments and switches
     """
 
     cli_args = get_cli_parser(instance_type).parse_args()
@@ -291,9 +384,18 @@ def get_parsed_cli(instance_type=InstanceType.NORMAL):
 
 class CLIArgs:
     """
-    # TODO: napoleon docstring
-    kentauros.init.cli.CLIArgs:
-    class that initialises CLI argument parsing and prepares them for reading
+    This class represents the parsed command line arguments and switches used
+    with a kentauros script. It stores the parsed CLI arguments between class
+    instantiations (in a class variable), so the CLI will be parsed only once.
+    It also provides simple method calls for getting settings from the parsed
+    CLI.
+
+    Attributes:
+        args: permanent (instance-independent) storage of parsed CLI arguments
+              (``None`` at first and set later - at first initialisation)
+
+    Arguments:
+        InstanceType itype: type of CLI to be created, parsed and stored
     """
 
     args = None
@@ -304,14 +406,25 @@ class CLIArgs:
 
     def get_debug(self):
         """
-        # TODO: napoleon docstring
+        This method simply returns whether the ``--debug`` or ``-d`` switch has
+        been set at the command line.
+
+        Returns:
+            bool: debug *on* or *off*
         """
 
         return self.args.debug
 
     def get_verby(self):
         """
-        # TODO: napoleon docstring
+        This method returns how often the ``--verbose`` or ``-v`` switch has
+        been set at the command line, subtracted from 2. This results in a
+        lowest verbosity level of *2* (comparable to ``--quiet``), a medium
+        verbosity level of *1* (normal verbosity) and a high verbosity level of
+        *0* (very verbose).
+
+        Returns:
+            int: verbosity level (0 to 2)
         """
 
         if (2 - self.args.verby) >= 0:
@@ -322,7 +435,11 @@ class CLIArgs:
 
     def get_priconf(self):
         """
-        # TODO: napoleon docstring
+        This method returns the preferred configuration file location (as an
+        instance of :py:class:`KtrConfType`).
+
+        Returns:
+            KtrConfType: type of the preferred configuration
         """
 
         # pylint: disable=unsubscriptable-object
@@ -336,42 +453,77 @@ class CLIArgs:
 
     def get_ktr_basedir(self):
         """
-        # TODO: napoleon docstring
+        This method returns kentauros BASEDIR specified by CLI argument.
+
+        Returns:
+            str:  specified BASEDIR
+
+        Returns:
+            None: no BASEDIR specified
         """
 
         return self.args.basedir
 
     def get_ktr_confdir(self):
         """
-        # TODO: napoleon docstring
+        This method returns kentauros CONFDIR specified by CLI argument.
+
+        Returns:
+            str:  specified CONFDIR
+
+        Returns:
+            None: no CONFDIR specified
         """
 
         return self.args.confdir
 
     def get_ktr_datadir(self):
         """
-        # TODO: napoleon docstring
+        This method returns kentauros DATADIR specified by CLI argument.
+
+        Returns:
+            str:  specified DATADIR
+
+        Returns:
+            None: no DATADIR specified
         """
 
         return self.args.datadir
 
     def get_ktr_packdir(self):
         """
-        # TODO: napoleon docstring
+        This method returns kentauros PACKDIR specified by CLI argument.
+
+        Returns:
+            str:  specified PACKDIR
+
+        Returns:
+            None: no PACKDIR specified
         """
 
         return self.args.packdir
 
     def get_ktr_specdir(self):
         """
-        # TODO: napoleon docstring
+        This method returns kentauros SPECDIR specified by CLI argument.
+
+        Returns:
+            str:  specified SPECDIR
+
+        Returns:
+            None: no SPECDIR specified
         """
 
         return self.args.specdir
 
     def get_action(self):
         """
-        # TODO: napoleon docstring
+        This method returns which package action has been specified at CLI.
+        If this is ``None``, something went wrong and no action type will be
+        returned.
+
+        Returns:
+            ActionType: type of action that will be executed
         """
 
         if 'action' in self.args:
@@ -380,7 +532,11 @@ class CLIArgs:
 
     def get_packages(self):
         """
-        # TODO: napoleon docstring
+        This method returns all the package names which were collected from
+        the CLI arguments.
+
+        Returns:
+            list: specified packages in a list
         """
 
         if 'package' in self.args:
@@ -390,7 +546,11 @@ class CLIArgs:
 
     def get_packages_all(self):
         """
-        # TODO: napoleon docstring
+        This method simply returns whether the ``--all`` or ``-a`` switch has
+        been set at the command line.
+
+        Returns:
+            bool: parse all packages *on* or *off*
         """
 
         if 'all' in self.args:
@@ -400,7 +560,11 @@ class CLIArgs:
 
     def get_force(self):
         """
-        # TODO: napoleon docstring
+        This method simply returns whether the ``--force`` or ``-f`` switch has
+        been set at the command line.
+
+        Returns:
+            bool: force package actions *on* or *off*
         """
 
         if 'force' in self.args:
@@ -411,7 +575,15 @@ class CLIArgs:
 
 class CLIArgsConfig(CLIArgs):
     """
-    # TODO: napoleon docstring
+    This class represents the parsed command line arguments and switches used
+    with a kentauros script (in this case: the ``ktr-config`` script). It stores
+    the parsed CLI arguments between class instantiations (in a class variable),
+    so the CLI will be parsed only once. It also provides simple method calls
+    for getting settings from the parsed CLI.
+
+    Attributes:
+        args: permanent (instance-independent) storage of parsed CLI arguments
+              (``None`` at first and set later - at first initialisation)
     """
 
     def __init__(self):
@@ -422,28 +594,44 @@ class CLIArgsConfig(CLIArgs):
 
     def get_config_section(self):
         """
-        # TODO: napoleon docstring
+        This method returns the configuration file section as specified by
+        CLI argument.
+
+        Returns:
+            str:  specified section
         """
 
         return self.args.section
 
     def get_config_key(self):
         """
-        # TODO: napoleon docstring
+        This method returns the configuration file key as specified by CLI
+        argument.
+
+        Returns:
+            str:  specified key
         """
 
         return self.args.key
 
     def get_config_value(self):
         """
-        # TODO: napoleon docstring
+        This method returns the configuration file value as specified by CLI
+        argument.
+
+        Returns:
+            str:  specified value
         """
 
         return self.args.value
 
     def get_confedit(self):
         """
-        # TODO: napoleon docstring
+        This method returns whether everything necessary for editing a config
+        file has been supplied at CLI (section, key, value).
+
+        Returns:
+            bool: ``True`` if everything was specified, ``False`` if not
         """
 
         return ("section" in self.args) and \
@@ -452,6 +640,11 @@ class CLIArgsConfig(CLIArgs):
 
 
 CLI_ARGS_DICT = dict()
+"""
+This dictionary contains a mapping from :py:class:`InstanceType` members to
+their respective :py:class:`CLIArgs` class or subclass constructors.
+"""
+
 CLI_ARGS_DICT[InstanceType.NORMAL] = CLIArgs
 CLI_ARGS_DICT[InstanceType.CREATE] = CLIArgs
 CLI_ARGS_DICT[InstanceType.CONFIG] = CLIArgsConfig
