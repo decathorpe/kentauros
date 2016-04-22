be1786b19b82ab06ad73c8b0311e2f1762ca045a
Fabio Valentini <decathorpe@gmail.com>
2016-04-22 13:57:11 +0200

    ktr/build/mock: code rework - wait for already running builds to finish - implement MockBuild helper class - more information about successful / failed builds - update stats - only include kentauros module in stats.

--- kentauros/build/mock.py
+++ kentauros/build/mock.py
@@ -4,12 +4,14 @@ to build binary packages from src.rpm packages.
 """
 
 
+import fcntl
 import glob
 import grp
 import os
 import subprocess
+import time
 
-from kentauros.instance import Kentauros, err, log, log_command
+from kentauros.instance import Kentauros, log, log_command
 from kentauros.build.builder import Builder
 
 
@@ -22,6 +24,91 @@ LOGPREFIX2 = " " * len(LOGPREFIX1) + "- "
 """This string specifies the prefix for lists printed through log and error
 functions, printed to stdout or stderr from inside this subpackage.
 """
+
+
+class MockBuild:
+    """
+    This helper class is used for the actual execution of mock.
+
+    Arguments:
+        str path:   path of the SRPM package that will be built
+        str dist:   chroot that the package will be built in
+
+    Attributes:
+        str path:   stores the path of the SRPM package that will be built
+        str dist:   stores the chroot that the package will be built in
+    """
+
+    def __init__(self, path: str, dist: str=None):
+        self.path = path
+        self.dist = dist
+
+    def get_command(self):
+        """
+        This method returns the argument list needed by the subprocess method
+        call, assembled from dist and path.
+
+        Returns:
+            list:   argument list for consumption by subprocess methods
+        """
+
+        ktr = Kentauros()
+
+        cmd = ["mock"]
+
+        # add --verbose or --quiet depending on settings
+        if (ktr.verby == 2) and not ktr.debug:
+            cmd.append("--quiet")
+        if (ktr.verby == 0) or ktr.debug:
+            cmd.append("--verbose")
+
+        # specify chroot if it has been set
+        if self.dist is not None:
+            cmd.append("-r")
+            cmd.append(self.dist)
+
+        # set .src.rpm file path
+        cmd.append(self.path)
+
+        return cmd
+
+
+    def build(self):
+        """
+        This method starts the mock build (and waits for already running builds
+        with the same chroot to finish before that).
+
+        Returns:
+            int:    return code of the subprocess call
+        """
+
+        dist_path = os.path.join("/var/lib/mock/", self.dist)
+        lock_path = os.path.join(dist_path, "buildroot.lock")
+
+        # wait for builds occupying the specified build chroot
+        if os.path.isdir(dist_path):
+            build_wait = True
+
+            while build_wait:
+                lock_file = open(lock_path, "a+")
+
+                try:
+                    fcntl.lockf(lock_file.fileno(),
+                                fcntl.LOCK_EX | fcntl.LOCK_NB)
+                except IOError:
+                    log(LOGPREFIX1 + \
+                        "The specified build chroot is busy, waiting.", 2)
+                    time.sleep(120)
+                else:
+                    build_wait = False
+                finally:
+                    lock_file.close()
+
+        cmd = self.get_command()
+        log_command(LOGPREFIX1, "mock", cmd, 2)
+
+        ret = subprocess.call(cmd)
+        return ret
 
 
 class MockBuilder(Builder):
@@ -88,52 +175,42 @@ class MockBuilder(Builder):
             for dist in dists:
                 log(LOGPREFIX2 + dist, 2)
 
-        # construct mock command
-        cmd = ["mock"]
+        # generate build queue
+        build_queue = list()
 
-        # add --verbose or --quiet depending on settings
-        if (ktr.verby == 2) and not ktr.debug:
-            cmd.append("--quiet")
-        if (ktr.verby == 0) or ktr.debug:
-            cmd.append("--verbose")
+        if dists == []:
+            build_queue.append(MockBuild(srpm))
+        else:
+            for dist in dists:
+                build_queue.append(MockBuild(srpm, dist))
 
+        # run builds in queue
         build_succ = list()
         build_fail = list()
 
-        mock_cmds = list()
-
-        if dists != []:
-            for dist in dists:
-                cmd_new = cmd.copy()
-                cmd_new.append("-r")
-                cmd_new.append(dist)
-                cmd_newest = cmd_new.copy()
-                cmd_newest.append(srpm)
-                mock_cmds.append(cmd_newest)
-        else:
-            cmd_new = cmd_new.copy()
-            cmd_new.append(srpm)
-            mock_cmds.append(cmd_newest)
-
-        for mock_cmd in mock_cmds:
-            log_command(LOGPREFIX1, "mock", mock_cmd, 1)
-            ret = subprocess.call(mock_cmd)
+        for build in build_queue:
+            ret = build.build()
             if ret:
-                build_fail.append(mock_cmd)
+                build_fail.append((build.path, build.dist))
             else:
-                build_succ.append(mock_cmd)
+                build_succ.append((build.path, build.dist))
 
         # remove source package if keep=False is specified
         if not self.package.conf.getboolean("mock", "keep"):
             os.remove(srpm)
 
-        if build_fail == []:
-            return True
-        else:
-            err(LOGPREFIX1 + "There are failed builds:")
-            for fail in build_fail:
-                err(LOGPREFIX2 + str(fail))
-            return False
+        if build_succ:
+            log(LOGPREFIX1 + "Build successes:", 2)
+            for success in build_succ:
+                log(LOGPREFIX2 + str(success), 2)
+
+        if build_fail:
+            log(LOGPREFIX1 + "Build failures:", 2)
+            for fails in build_fail:
+                log(LOGPREFIX2 + str(fails), 2)
+
+        return not build_fail
+
 
     def export(self) -> bool:
         """
