b7bb5e78c67d98e7a4f6a73373fec1347bdb1fce
Fabio Valentini <decathorpe@gmail.com>
2016-04-29 10:55:21 +0200

    ktr/instance: code refactoring - move 1-file-subpackage to kentauros submodule - add method equivalents of functions to Kentauros class

--- kentauros/instance.py
+++ kentauros/instance.py
@@ -1,5 +1,5 @@
 """
-This subpackage contains the :py:class:`Kentauros` class, which holds
+This submodule contains the :py:class:`Kentauros` class, which holds
 configuration values parsed from CLI arguments, environment variables and
 configuration files. The implementation makes sure that command line arguments,
 environment variables and configuration files are parsed only once per program
@@ -56,6 +56,78 @@ class Kentauros:
 
         if "conf" not in self.saved_state:
             self.conf = ktr_get_conf(itype)
+
+    def dbg(self, msg: str):
+        """
+        This method prints messages with a "DEBUG: " prefix to stdout, but
+        only if the ``KTR_DEBUG`` environment variable has been set or the
+        ``--debug`` or ``-d`` flag has been supplied to the CLI.
+
+        Arguments:
+            str msg: debug message to be printed
+        """
+
+        if self.debug:
+            print("DEBUG: " + str(msg))
+
+    def err(self, msg: str):
+        """
+        This method prints messages with an "ERROR: " prefix to standard error
+        output, regardless of environment variables and CLI settings supplied.
+
+        Arguments:
+            str msg: error message to be printed
+        """
+
+        self.log("ERROR: " + msg, 2)
+
+    def log(self, msg: str, pri: int=2):
+        """
+        This method prints messages to standard output, depending on the
+        priority argument and the verbosity level determined from environment
+        variables and CLI switches.
+
+        Invocation with
+
+        * ``pri=2`` (which is the default) will always print the attached
+          message
+        * ``pri=1`` will print messages when verbosity is set to 1
+        * ``pri=0`` will print messages when verbosity is set to 0 or when
+          debugging is enabled
+
+        Arguments:
+            str msg: message that will be printed
+            int pri: message priority (0-2, where 0 is lowest and 2 is highest)
+        """
+
+        if (pri >= self.verby) or self.debug:
+            print(msg)
+
+    def log_command(self, prefix1: str, basename: str,
+                    cmdlist: list, pri: int=2):
+        """
+        This method prints commands that are then executed by use of the
+        :py:func:`subprocess.call` or :py:func:`subprocess.check_output`
+        functions. Its priority behaviour is the same as the :py:func:`log`
+        function's.
+
+        Arguments:
+            str prefix1:    module-wide prefix string
+            str basename:   command base name
+            list cmdlist:   list of strings, as passed to :py:mod:``subprocess``
+                            functions
+            int pri:        message priority (0-2, where 0 is lowest and 2 is
+                            highest)
+        """
+
+        cmdstr = ""
+        prefix2 = " " * len(prefix1)
+
+        for cmd in cmdlist:
+            cmdstr += (cmd + " ")
+
+        self.log(prefix1 + basename + " command:", pri)
+        self.log(prefix2 + cmdstr, pri)
 
 
 def dbg(msg: str):
