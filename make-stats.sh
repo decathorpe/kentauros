8f5207b8a4bbe211ca85ea1cfcb2c9bdfc129cdb
Fabio Valentini <decathorpe@gmail.com>
2016-11-07 17:59:25 +0100

    meta: update pylint options

--- make-stats.sh
+++ make-stats.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 # generate pylint statistics
-python3-pylint --enable=all -f html ./kentauros ./scripts/ktr > meta/pylint_stats.html
+python3-pylint --enable=all --jobs=4 -f html ./kentauros ./scripts/ktr > meta/pylint_stats.html
