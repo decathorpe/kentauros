4cf822436a5b2055492c14da0bb245502643405f
Fabio Valentini <decathorpe@gmail.com>
2017-04-30 23:24:06 +0200

    make-srpm.sh: update script

--- make-srpm.sh
+++ make-srpm.sh
@@ -6,15 +6,16 @@ VERSION=$(python3 -c 'from kentauros.definitions import KTR_VERSION; print(KTR_V
 mkdir -p $HOME/rpmbuild/SOURCES
 mkdir -p $HOME/rpmbuild/SPECS
 
-git archive --format=tar.gz --prefix=$NAME-$VERSION/ HEAD > $HOME/rpmbuild/SOURCES/$VERSION.tar.gz
+git archive --format=tar.gz --prefix=$NAME-$VERSION/ HEAD > $HOME/rpmbuild/SOURCES/$NAME-$VERSION.tar.gz
 
 cp ./$NAME.spec $HOME/rpmbuild/SPECS/
 
 cd $HOME/rpmbuild/SPECS/
 rpmbuild -bs $NAME.spec
+rm $NAME.spec
+
+cd $HOME/rpmbuild/SOURCES/
+rm $NAME*.tar.gz
 
 cd $HOME/rpmbuild/SRPMS/
-mv *.rpm $HOME/
-
-cd $HOME
-rm -r rpmbuild
+mv kentauros*.rpm $HOME/
