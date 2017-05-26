58bd9d6ad1f26b51b74cdad0cf9f91dca9a3e0f0
Fabio Valentini <decathorpe@gmail.com>
2017-05-26 14:27:35 +0200

    meta: update stats generator for new pylint (without html output)

--- make-stats.sh
+++ make-stats.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 # generate pylint statistics
-pylint-3 --enable=all -f html ./kentauros ./scripts/ktr > meta/pylint_stats.html
+pylint-3 --enable=all ./kentauros ./scripts/ktr > meta/pylint_stats.txt
