22fad176b1a325742cc0b7a483a30a30fa0978a7
Fabio Valentini <decathorpe@gmail.com>
2017-12-13 17:26:30 +0100

    meta: update make-stats shell script

--- make-stats.sh
+++ make-stats.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 # generate pylint statistics
-pylint-3 --enable=all --disable=no-else-return,len-as-condition \
+pylint-3 --enable=all --disable=no-else-return,len-as-condition,locally-disabled,suppressed-message \
     ./kentauros ./scripts/ktr > meta/pylint_stats.txt
