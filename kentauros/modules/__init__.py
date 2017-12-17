7a5b6730385db60b0908d4b7e63dde8ad0adf969
Fabio Valentini <decathorpe@gmail.com>
2017-12-17 17:55:30 +0100

    ktr/modules: fix circular deps

--- kentauros/modules/__init__.py
+++ kentauros/modules/__init__.py
@@ -2,17 +2,17 @@
 This subpackage contains all plug-able kentauros modules.
 """
 
-
 from ..context import KtrContext
 from ..definitions import PkgModuleType, SourceType, ConstructorType, BuilderType, UploaderType
 from ..package import KtrPackage
+from ..result import KtrResult
+
+from .builder import get_builder
+from .constructor import get_constructor
+from .sources import get_source
+from .uploader import get_uploader
 
 from .module import KtrModule
-from .package import PackageModule
-from .sources import get_source
-from .constructor import get_constructor
-from .builder import get_builder
-from .uploader import get_uploader
 
 
 def _get_pkg_module(mtype: PkgModuleType, stype, package: KtrPackage,
@@ -61,3 +61,126 @@ def get_module(mtype: PkgModuleType, mimpl: str, package: KtrPackage,
     pkg_module = _get_pkg_module(mtype, pkg_module_type, package, context)
 
     return pkg_module
+
+
+class PackageModule(KtrModule):
+    NAME = "Package"
+
+    def __init__(self, package: KtrPackage, context: KtrContext):
+        super().__init__(package, context)
+
+        self.actions["chain"] = self.execute
+        self.actions["import"] = self.imports
+        self.actions["status"] = self.status_string
+
+        self.modules = list()
+
+        module_types = (PkgModuleType[i.upper()] for i
+                        in self.package.conf.get("main", "modules").split(","))
+
+        if module_types == "":
+            module_types = []
+
+        for module_type in module_types:
+            module_impl = self.package.conf.get("modules", str(module_type.name).lower())
+            mod = get_module(module_type, module_impl.upper(), self.package, self.context)
+            self.modules.append(mod)
+
+    def name(self):
+        return "{} {}".format(self.NAME, self.package.name)
+
+    def __str__(self) -> str:
+        return "{} '{}'".format(self.NAME, self.package.name)
+
+    def execute(self) -> KtrResult:
+        ret = KtrResult(name=self.name())
+        success = True
+
+        for mod in self.modules:
+            assert isinstance(mod, KtrModule)
+            res = mod.execute()
+            ret.collect(res)
+            success = success and res.success
+
+            if self.context.get_argument("force"):
+                continue
+            if not res.success:
+                break
+
+        return ret.submit(success)
+
+    def clean(self) -> KtrResult:
+        ret = KtrResult(name=self.name())
+        success = True
+
+        for mod in self.modules:
+            assert isinstance(mod, KtrModule)
+            res = mod.clean()
+            ret.collect(res)
+            success = success and res.success
+
+        return ret.submit(success)
+
+    def imports(self) -> KtrResult:
+        ret = KtrResult(name=self.name())
+        success = True
+
+        for mod in self.modules:
+            assert isinstance(mod, KtrModule)
+            res = mod.imports()
+            ret.collect(res)
+            success = success and res.success
+
+        return ret.submit(success)
+
+    def status(self) -> KtrResult:
+        ret = KtrResult(name=self.name())
+        success = True
+
+        for mod in self.modules:
+            assert isinstance(mod, KtrModule)
+            res = mod.status()
+            ret.collect(res)
+            success = success and res.success
+
+        return ret.submit(success)
+
+    def status_string(self) -> KtrResult:
+        ret = KtrResult(name=self.name())
+        string = str()
+        success = True
+
+        res = self.package.status_string()
+        ret.collect(res)
+
+        if ret.success:
+            string += ret.value
+
+        for mod in self.modules:
+            assert isinstance(mod, KtrModule)
+            res = mod.status_string()
+            ret.collect(res)
+
+            if ret.success:
+                string += ret.value
+
+            success = success and res.success
+
+        return ret.submit(success)
+
+    def verify(self) -> KtrResult:
+        ret = KtrResult(name=self.name())
+        success = True
+
+        res = self.package.verify()
+        ret.collect(res)
+
+        success = success and res.success
+
+        for mod in self.modules:
+            assert isinstance(mod, KtrModule)
+            res = mod.verify()
+            ret.collect(res)
+            success = success and res.success
+
+        return ret.submit(success)
