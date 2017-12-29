898bc7b34aff203515cd956966a1fcc49551120b
Fabio Valentini <decathorpe@gmail.com>
2017-12-29 10:17:13 +0100

    ktr/sources/url: use a ShellCommand subclass for WGet calls

--- kentauros/modules/sources/url.py
+++ kentauros/modules/sources/url.py
@@ -1,15 +1,24 @@
 import os
-import subprocess as sp
 
 from .abstract import Source
 from ...conntest import is_connected
 from ...context import KtrContext
 from ...package import KtrPackage
 from ...result import KtrResult
+from ...shellcmd import ShellCommand
 from ...validator import KtrValidator
 
 
-# TODO: Use a ShellCommand subclass for wget commands
+class WGetCommand(ShellCommand):
+    NAME = "wget Command"
+
+    def __init__(self, path: str, *args, binary=None):
+        if binary is None:
+            self.exec = "wget"
+        else:
+            self.exec = binary
+
+        super().__init__(path, self.exec, *args)
 
 
 class UrlSource(Source):
@@ -93,37 +102,19 @@ class UrlSource(Source):
             return ret.submit(False)
 
         # construct wget commands
-        cmd = ["wget"]
+        cmd = ["wget", self.get_orig(), "-O", self.dest]
 
-        # add --verbose or --quiet depending on settings
-        if self.context.debug():
-            cmd.append("--verbose")
-        else:
-            cmd.append("--quiet")
+        res = WGetCommand(".", *cmd).execute()
+        ret.collect(res)
 
-        # set origin and destination
-        cmd.append(self.get_orig())
-        cmd.append("-O")
-        cmd.append(self.dest)
-
-        # wget source from origin to destination
-        ret.messages.cmd(cmd)
-        res: sp.CompletedProcess = sp.run(cmd,
-                                          stdout=sp.PIPE,
-                                          stderr=sp.STDOUT)
-
-        if res.returncode != 0:
-            ret.messages.lst("Sources could not be downloaded successfully. wget output:",
-                             res.stdout.decode().split("\n"))
+        if not res.success:
+            ret.messages.lst("Sources could not be downloaded successfully. CLI output:", res.value)
             return ret.submit(False)
 
-        success = (ret == 0)
-
-        if success:
-            self.last_version = self.package.get_version()
-
+        self.last_version = self.package.get_version()
         ret.state["source_files"] = [os.path.basename(self.get_orig())]
-        return ret.submit(success)
+
+        return ret.submit(True)
 
     def update(self) -> KtrResult:
         ret = KtrResult(True, name=self.name())
