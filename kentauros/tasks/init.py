871ad4c315c4eb33946e6ba82189cfed82804750
Fabio Valentini <decathorpe@gmail.com>
2018-01-02 15:50:04 +0100

    ktr: merge bootstrap module into KtrInitTask class

--- kentauros/tasks/init.py
+++ kentauros/tasks/init.py
@@ -1,7 +1,27 @@
+import os
+
 from .meta import KtrMetaTask
-from ..bootstrap import ktr_bootstrap
 from ..context import KtrContext
 from ..result import KtrResult
+
+
+def ktr_mkdirp(path: str) -> KtrResult:
+    ret = KtrResult(name="bootstrap")
+
+    if os.path.exists(path):
+        if os.access(path, os.W_OK):
+            return ret.submit(True)
+        else:
+            ret.messages.err(path + " can't be written to.")
+            return ret.submit(False)
+    else:
+        ret.messages.log(path + " directory doesn't exist and will be created.")
+        try:
+            os.makedirs(path)
+        except OSError:
+            ret.messages.err(path + " directory could not be created.")
+            return ret.submit(False)
+        return ret.submit(True)
 
 
 class KtrInitTask(KtrMetaTask):
@@ -9,4 +29,18 @@ class KtrInitTask(KtrMetaTask):
         self.context = context
 
     def execute(self) -> KtrResult:
-        return ktr_bootstrap(self.context)
+        ret = KtrResult(name="bootstrap")
+
+        # TODO: create kentaurosrc file from template
+
+        for path in [self.context.get_basedir(), self.context.get_confdir(),
+                     self.context.get_datadir(), self.context.get_expodir(),
+                     self.context.get_packdir(), self.context.get_specdir()]:
+
+            res = ktr_mkdirp(path)
+            ret.collect(res)
+
+            if not res.success:
+                return ret.submit(False)
+
+        return ret.submit(True)
